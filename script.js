9d6bc39fd1dcede6522c085ea0b4dd266126d8d7
Bharat Mishra <mishrabm2004@gmail.com>
2021-06-29 18:43:27 +0530

    new files updated

---
+++ script.js
@@ -0,0 +1,9 @@
+function transitionOnLoad() {
+
+   const trans = document.getElementsByTagName('body')[0];
+
+    
+   trans.setAttribute("class", "loading");
+
+   document.getElementById('name')
+}
