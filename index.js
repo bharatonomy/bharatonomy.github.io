f822084e66be40c1527ebc01116696ad93783d4a
Bharat Mishra <mishrabm2004@gmail.com>
2021-07-16 22:22:36 +0530

    new updates

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello');
+
+const express = require('express')
+const path = require('path');
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join (__dirname,'templates/main.html'))
+})
+
+app.get('/contact',(req, res)=>{
+
+    res.sendFile(path.join(__dirname,'templates/contact.html'))
+})
+
+
+app.get('/work', (req , res)=>{
+
+res.sendFile(path.join(__dirname,'templates/portfolio.html'))
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
