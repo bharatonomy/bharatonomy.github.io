0fce77cab61966576b1b39830a4558d0369c1dea
TheBharatMishra <mishrabm2004@gmail.com>
2024-02-17 01:10:13 +0530

    Might Work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
 <h1>Bharat Mishra</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>About <a href="{base}/about">kit.svelte.dev</a> to read the documentation</p>
