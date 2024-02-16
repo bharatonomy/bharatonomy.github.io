0ebc1616989548012f020dd9604df00bb973ee25
TheBharatMishra <mishrabm2004@gmail.com>
2024-02-17 01:29:10 +0530

    Deployment Branch

---
+++ _app/immutable/entry/start.BwcaKHpc.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BG_CnMH5.js";export{t as start};
--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import { base } from '$app/paths';
-</script>
-
-<h1>Bharat Mishra</h1>
-<p>About <a href="{base}/about">kit.svelte.dev</a> to read the documentation</p>
